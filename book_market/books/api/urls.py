89a55edf1bba27b13aa93c7e56f156a609bb15fe
bilalylmz39 <yusufbilalylmz29@gmail.com>
2024-06-30 04:08:12 +0300

    refactor: Exclude 'book' field from CommentSerializer

--- book_market/books/api/urls.py
+++ book_market/books/api/urls.py
@@ -4,4 +4,6 @@ from books.api import views as api_views
 urlpatterns = [
     path('books/', api_views.BookListCreateAPIView.as_view(), name='book-list-create'),
     path('books/<int:pk>/', api_views.BookDetailAPIView.as_view(), name='book-details'),
+    path('books/<int:book_id>/add_comment/', api_views.CommentCreateAPIView.as_view(), name='comment-create'),
+    path('comments/<int:pk>/', api_views.CommentDetailAPIView.as_view(), name='comment-details'),
 ]
--- book_market/books/api/views.py
+++ book_market/books/api/views.py
@@ -3,15 +3,32 @@ from books.api.serializers import BookSerializer, CommentSerializer
 from books.models import Book, Comment
 from rest_framework.mixins import ListModelMixin, CreateModelMixin
 from rest_framework import generics
+from rest_framework.generics import get_object_or_404
 
 #concrete view
 class BookListCreateAPIView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+
 class BookDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer    
+
+
+class CommentCreateAPIView(generics.CreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+    def perform_create(self, serializer):
+        book_id = self.kwargs.get('book_id')
+        book = get_object_or_404(Book, pk=book_id)
+        serializer.save(book=book)
+
+
+class CommentDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
 
 
 
